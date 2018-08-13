6970e33a50453b31671e473b7faf9962c51c4474
itReverie <brenda@itreverie.com>
2018-08-13 13:10:35 -0400

    Removing unnecesary comments

--- data/resolvers.js
+++ data/resolvers.js
@@ -2,27 +2,21 @@ import { PubSub, withFilter } from 'apollo-server';
 import { Message } from '../models';
 require('dotenv').config();
 
-
 const MESSAGE_CREATED = 'MESSAGE_CREATED';
 const MESSAGE_UPDATED = 'MESSAGE_UPDATED';
 
 const pubsub = new PubSub();
 
-// Define resolvers
 const resolvers = {
     Query: {
-        // Fetch all messages
         async allMessages() {
             return await Message.all({order:[['id', 'DESC']]});
         },
-  
-        // Get a message by ID
         async fetchMessage(_, { id }) {
             return await Message.findById(id);
         },
     },
     Mutation: {
-        // Create new message
         async createMessage(_, { text }) {
             await Message.create({
                 text
@@ -35,11 +29,8 @@ const resolvers = {
                 return message;
             });
         },
-        // Update a particular message
         async updateMessage(_, { id, text, isFavorite}) {
-            // fetch the user by it ID
             const message = await Message.findById(id);
-            // Update the user
             await message.update({
                 text,
                 isFavorite
@@ -53,17 +44,15 @@ const resolvers = {
     },
     Subscription: {
         messageCreated: {
-          // Additional event labels can be passed to asyncIterator creation
           subscribe: () => pubsub.asyncIterator([MESSAGE_CREATED]),
         },
         messageUpdated: {
-            // Additional event labels can be passed to asyncIterator creation
             subscribe: withFilter(
-                                        () => pubsub.asyncIterator([MESSAGE_UPDATED]),
+                                  () => pubsub.asyncIterator([MESSAGE_UPDATED]),
                                         (payload, variables) => {
-                                            return payload.messageUpdated.id === variables.id;
-                                           },
-                                        ),
+                                                return payload.messageUpdated.id === variables.id;
+                                            },
+                                  ),
           },
     }
 }
